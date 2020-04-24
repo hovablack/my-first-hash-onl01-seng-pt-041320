d80bc155ca2013c46c8334a16462558453609ac3
Osi Ejedawe <osi.ejedawe@hotmail.com>
2020-04-24 04:33:50 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  myhash = {"game" => "Dark Souls", "developer => "From Software"}# use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  your_hash = {"game" => "Dark Souls", "developer => "From Software"}
 
 
 end
